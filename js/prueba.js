5496379ef7af7e9591b1d03564ba96139a87d405
GavilanesJr <64985296+JorgeGavilanes@users.noreply.github.com>
2020-07-27 20:11:52 -0500

    Add files via upload

--- js/prueba.js
+++ js/prueba.js
@@ -1,4 +1,4 @@
-var cont;
+var x = 0;
 function btnPlay(){
     document.getElementById("btnPlay").style.display="none";
     return document.getElementById("1").style.display="flex";
@@ -7,6 +7,8 @@ function correct1(){
     document.getElementById("iH1").src="media/happy.svg";
     document.getElementById("1").style.display="none";
     document.getElementById("2").style.display="flex";
+    x += 1;
+    return x;
 }
 function incorrect1(){
     document.getElementById("iH1").src="media/sad.svg";
@@ -18,6 +20,8 @@ function correct2(){
     document.getElementById("iH2").src="media/happy.svg";
     document.getElementById("2").style.display="none";
     document.getElementById("3").style.display="flex";
+    x += 1;
+    return x;
 }
 function incorrect2(){
     document.getElementById("iH2").src="media/sad.svg";
@@ -30,6 +34,8 @@ function correct3(){
     document.getElementById("iH3").src="media/happy.svg";
     document.getElementById("3").style.display="none";
     document.getElementById("4").style.display="flex";
+    x += 1;
+    return x;
 }
 function incorrect3(){
     document.getElementById("iH3").src="media/sad.svg";
@@ -40,7 +46,9 @@ function incorrect3(){
 function correct4(){
     document.getElementById("iH4").src="media/happy.svg";
     document.getElementById("4").style.display="none";
-    return document.getElementById("5").style.display="flex";
+    document.getElementById("5").style.display="flex";
+    x += 1;
+    return x;
 }
 function incorrect4(){
     document.getElementById("iH4").src="media/sad.svg";
@@ -53,6 +61,8 @@ function correct5(){
     document.getElementById("iH5").src="media/happy.svg";
     document.getElementById("5").style.display="none";
     document.getElementById("6").style.display="flex";
+    x += 1;
+    return x;
 }
 function incorrect5(){
     document.getElementById("iH5").src="media/sad.svg";
@@ -64,6 +74,8 @@ function correct6(){
     document.getElementById("iH6").src="media/happy.svg";
     document.getElementById("6").style.display="none";
     document.getElementById("7").style.display="flex";
+    x += 1;
+    return x;
 }
 function incorrect6(){
     document.getElementById("iH6").src="media/sad.svg";
@@ -76,6 +88,8 @@ function correct7(){
     document.getElementById("iH7").src="media/happy.svg";
     document.getElementById("7").style.display="none";
     document.getElementById("8").style.display="flex";
+    x += 1;
+    return x;
 }
 function incorrect7(){
     document.getElementById("iH7").src="media/sad.svg";
@@ -87,6 +101,8 @@ function correct8(){
     document.getElementById("iH8").src="media/happy.svg";
     document.getElementById("8").style.display="none";
     document.getElementById("9").style.display="flex";
+    x += 1;
+    return x;
 }
 function incorrect8(){
     document.getElementById("iH8").src="media/sad.svg";
@@ -99,6 +115,8 @@ function correct9(){
     document.getElementById("iH9").src="media/happy.svg";
     document.getElementById("9").style.display="none";
     document.getElementById("10").style.display="flex";
+    x += 1;
+    return x;
 }
 function incorrect9(){
     document.getElementById("iH9").src="media/sad.svg";
@@ -108,16 +126,27 @@ function incorrect9(){
 }
 function correct10(){
     document.getElementById("iH10").src="media/happy.svg";
+    x += 1;
+    return x;
+    if(x>7){
+        document.getElementById("10").style.display="none";
+        document.getElementById("dceW").style.display="block";
+        document.getElementById("spanContW").innerHTML = x;
+    }
+    if(x<7){
+        document.getElementById("10").style.display="none";
+        document.getElementById("dceL").style.display="block";
+    }
 }
 function incorrect10(){
     document.getElementById("iH10").src="media/sad.svg";
-}
-function changeWin(){
-    if("cont()">7){
+    if(x>7){
         document.getElementById("10").style.display="none";
-    document.getElementById("dceW").style.display="flex";
-    }else{
+        document.getElementById("dceW").style.display="block";
+        document.getElementById("spanContW").innerHTML = x;
+    }
+    if(x<7){
         document.getElementById("10").style.display="none";
-    document.getElementById("dceL").style.display="flex";
+        document.getElementById("dceL").style.display="block";
     }
 }
