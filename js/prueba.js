4d0fbc4c15a49b773f23e61b99baf75a1fe6290c
GavilanesJr <64985296+JorgeGavilanes@users.noreply.github.com>
2020-09-22 23:41:07 -0500

    Add files via upload

--- js/prueba.js
+++ js/prueba.js
@@ -1,161 +1,52 @@
-var x = 0;
-var inicio = new Audio();
-var integrantes = new Audio();
-var gana = new Audio();
-var pierde = new Audio();
+var inicio = new Audio(), integrantes = new Audio(), gana = new Audio(), pierde = new Audio();
 inicio.src="inicio.mp3";
 integrantes.src="integrantes.mp3";
 gana.src="gana.mp3";
 pierde.src="pierde.mp3";
 function btnPlay(){
     document.getElementById("btnPlay").style.display="none";
-    return document.getElementById("1").style.display="flex";
+    return document.getElementById(1).style.display="flex";
 }
-function correct1(){
-    document.getElementById("iH1").src="media/happy.svg";
-    document.getElementById("1").style.display="none";
-    document.getElementById("2").style.display="flex";
-    x += 1;
-    return x;
+let n = 1, cont = 0;
+function correct() {
+    cont += 1;
+    document.getElementById("iH-img"+n).src="media/happy.svg";
+    document.getElementById(n).style.display="none";
+    n += 1;
+    moreThanTen();
+    showHappy();
+    winnerOrLoser();
 }
-function incorrect1(){
-    document.getElementById("iH1").src="media/sad.svg";
-    document.getElementById("1").style.display="none";
-    document.getElementById("2").style.display="flex";
-    document.getElementById("iH2").src="media/sad.svg";
+function incorrect() {
+    document.getElementById("iH-img"+n).src="media/sad.svg";
+    document.getElementById(n).style.display="none";
+    n += 1;
+    moreThanTen();
+    showSad();
+    winnerOrLoser();
 }
-function correct2(){
-    document.getElementById("iH2").src="media/happy.svg";
-    document.getElementById("2").style.display="none";
-    document.getElementById("3").style.display="flex";
-    x += 1;
-    return x;
-}
-function incorrect2(){
-    document.getElementById("iH2").src="media/sad.svg";
-    document.getElementById("2").style.display="none";
-    document.getElementById("3").style.display="flex";
-    document.getElementById("iH3").src="media/sad.svg";
-}
-
-function correct3(){
-    document.getElementById("iH3").src="media/happy.svg";
-    document.getElementById("3").style.display="none";
-    document.getElementById("4").style.display="flex";
-    x += 1;
-    return x;
-}
-function incorrect3(){
-    document.getElementById("iH3").src="media/sad.svg";
-    document.getElementById("3").style.display="none";
-    document.getElementById("4").style.display="flex";
-    document.getElementById("iH4").src="media/sad.svg";
-}
-function correct4(){
-    document.getElementById("iH4").src="media/happy.svg";
-    document.getElementById("4").style.display="none";
-    document.getElementById("5").style.display="flex";
-    x += 1;
-    return x;
-}
-function incorrect4(){
-    document.getElementById("iH4").src="media/sad.svg";
-    document.getElementById("4").style.display="none";
-    document.getElementById("5").style.display="flex";
-    document.getElementById("iH5").src="media/sad.svg";
-}
-
-function correct5(){
-    document.getElementById("iH5").src="media/happy.svg";
-    document.getElementById("5").style.display="none";
-    document.getElementById("6").style.display="flex";
-    x += 1;
-    return x;
-}
-function incorrect5(){
-    document.getElementById("iH5").src="media/sad.svg";
-    document.getElementById("5").style.display="none";
-    document.getElementById("6").style.display="flex";
-    document.getElementById("iH6").src="media/sad.svg";
-}
-function correct6(){
-    document.getElementById("iH6").src="media/happy.svg";
-    document.getElementById("6").style.display="none";
-    document.getElementById("7").style.display="flex";
-    x += 1;
-    return x;
-}
-function incorrect6(){
-    document.getElementById("iH6").src="media/sad.svg";
-    document.getElementById("6").style.display="none";
-    document.getElementById("7").style.display="flex";
-    document.getElementById("iH7").src="media/sad.svg";
-}
-
-function correct7(){
-    document.getElementById("iH7").src="media/happy.svg";
-    document.getElementById("7").style.display="none";
-    document.getElementById("8").style.display="flex";
-    x += 1;
-    return x;
-}
-function incorrect7(){
-    document.getElementById("iH7").src="media/sad.svg";
-    document.getElementById("7").style.display="none";
-    document.getElementById("8").style.display="flex";
-    document.getElementById("iH8").src="media/sad.svg";
-}
-function correct8(){
-    document.getElementById("iH8").src="media/happy.svg";
-    document.getElementById("8").style.display="none";
-    document.getElementById("9").style.display="flex";
-    x += 1;
-    return x;
-}
-function incorrect8(){
-    document.getElementById("iH8").src="media/sad.svg";
-    document.getElementById("8").style.display="none";
-    document.getElementById("9").style.display="flex";
-    document.getElementById("iH9").src="media/sad.svg";
-}
-
-function correct9(){
-    document.getElementById("iH9").src="media/happy.svg";
-    document.getElementById("9").style.display="none";
-    document.getElementById("10").style.display="flex";
-    x += 1;
-    return x;
-}
-function incorrect9(){
-    document.getElementById("iH9").src="media/sad.svg";
-    document.getElementById("9").style.display="none";
-    document.getElementById("10").style.display="flex";
-    document.getElementById("iH10").src="media/sad.svg";
-}
-function correct10(){
-    document.getElementById("iH10").src="media/happy.svg";
-    x += 1;
-    if(x=>7){
-        document.getElementById("10").style.display="none";
-        document.getElementById("dceW").style.display="block";
-        document.getElementById("spanContW").innerHTML = x;
-    }
-    if(x<7){
-        document.getElementById("10").style.display="none";
-        document.getElementById("dceW").style.display="none";
-        document.getElementById("dceL").style.display="block";
+function moreThanTen(){
+    if(n <= 10){
+        document.getElementById(n).style.display="flex";
     }
 }
-function incorrect10(){
-    document.getElementById("iH10").src="media/sad.svg";
-    if(x=>7){
-        document.getElementById("10").style.display="none";
-        document.getElementById("dceW").style.display="block";
-        document.getElementById("spanContW").innerHTML = x;
+function showHappy(){
+    if(n <= 10){
+        document.getElementById("iH-img"+n).src="media/happy.svg";
     }
-    if(x<7){
-        document.getElementById("10").style.display="none";
-        document.getElementById("dceW").style.display="none";
-        document.getElementById("dceL").style.display="block";
+}
+function showSad(){
+    if(n <= 10){
+        document.getElementById("iH-img"+n).src="media/sad.svg";
+    }
+}
+function winnerOrLoser(){
+    if(n > 10){
+        if(cont >= 7){
+            document.getElementById("dceW").style.display="block";
+            document.getElementById("spanContW").innerHTML = cont;
+        }else{
+            document.getElementById("dceL").style.display="block";
+        }
     }
 }
